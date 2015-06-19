b8eb891800f3f2e8a241c501786c50eba1d43c6f
Emily Kapor <emily@emilyaviva.com>
2015-06-19 01:11:35 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# basic-node-server
+
+The file server.js implements a basic Node server. It serves the static file index.html, and on 404 errors brings up notfound.html.
---
+++ server.js
@@ -0,0 +1,38 @@
+var http = require('http');
+var fs = require('fs');
+
+var staticPage = './index.html';
+var staticPageContentType = 'text/html';
+var failPage = './notfound.html';
+var failPageContentType = 'text/html';
+
+var port = process.env.PORT || 3000;
+
+var server = http.createServer(function(req, res) {
+
+  var path = '.' + req.url;
+  if (path == './') {
+    path = staticPage;
+  }
+
+  fs.readFile(path, function(err, file) {
+    if (err) {
+      if (err.code == 'ENOENT') {
+        fs.readFile(failPage, function(err, file) {
+          res.writeHead(200, {'Content-Type': failPageContentType});
+          res.end(file);
+        });
+      } else {
+        res.writeHead(500);
+        res.end('An internal error occurred. Error: ' + err.code);
+      }
+    } else {
+      res.writeHead(200, {'Content-Type': staticPageContentType});
+      res.end(file);
+    };
+  })
+});
+
+server.listen(port, function() {
+  console.log('server is listening on port ' + port);
+});
